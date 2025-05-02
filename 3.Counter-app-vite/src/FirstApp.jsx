6137e6a333cb7d056cda11d0b36b2e160aa02fd6
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-05-01 23:50:28 -0600

    add style titulo y subtitulo como prop numeor

--- 3.Counter-app-vite/src/FirstApp.jsx
+++ 3.Counter-app-vite/src/FirstApp.jsx
@@ -3,14 +3,14 @@
  //props es un objeto que contiene las propiedades que se le pasan al componente
  //Permite que del componente padre se le pasen propiedades al componente hijo
 // export const FirstApp = ({title='hola, soy Goku'}) es el titulo por defecto que se le pasa al componente
-    export const FirstApp = ({title='hola, soy Goku'}) => {
+    export const FirstApp = ({title, subTitle}) => {
 
     // console.log(title);
 
     return (
         <>
         <h1>{title}</h1>
-        <p>Soy la mejor programadora</p>
+        <p>{subTitle +1}</p>
         </>
     );
 }
