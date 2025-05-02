3aafd6acd65e3ec6d2d6e8378d9ac318c3430fc4
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-05-02 08:36:23 -0600

    add funcion como parametro y valor de otra funcion

--- 3.Counter-app-vite/src/CounterApp.jsx
+++ 3.Counter-app-vite/src/CounterApp.jsx
@@ -1,11 +1,21 @@
 import PropTypes from "prop-types"
+
+//se define fuera de la funcion para que no se vuelva a crear cada vez que se renderiza el componente
+const handleAdd = (event) {
+    console.log(event);
+}
+
+
 export const CounterApp = ({value}) => {
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{value}</h2>
-{/* //lo que esta a lado del onClikc es una expeersion de javascript, y lo que esta entre llaves es un objeto, por eso se le pone el onClick={} */}
-             <button onClick={() => console.log('+1')}>
+{/* handleAdd es una funcion que se ejecuta cuando se hace click en el boton, y recibe el evento como parametro */}
+             {/* <button onClick={handleAdd}> */}
+
+             {/* se puede pasar una funcion como parametro, y se le puede pasar un segundo parametro, que es el valor que queremos que tenga la funcion al hacer click */}
+                <button onClick={handleAdd}>
                 +1
              </button>
         </>
