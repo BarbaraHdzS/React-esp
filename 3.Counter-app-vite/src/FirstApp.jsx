bf4603e604c751eefb3ed1c22173a671130df873
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-05-01 23:08:26 -0600

    add name

--- 3.Counter-app-vite/src/FirstApp.jsx
+++ 3.Counter-app-vite/src/FirstApp.jsx
@@ -1,22 +1,31 @@
-// //Fragment Se utiliza para agrupar varios elementos sin envolverlos en un div u otro elemento HTML.
-// import { Fragment } from "react";
+// //siempre y cuando haya un objeto va a funcionar
+// export const FirstApp = () => { 
+//     return (
+//         <h1>{1+1}</h1>
+//     );
+// }
 
+//se puede todo menos booleanos y null
+// const newName = "Barbara";
+// const newNumber = [1,2,3,4,5,6,7,8,9,10];
 // export const FirstApp = () => {
 //     return (
-//         //si quieres meter varios elementos (parrafos) debes poner div o Fragment si no lo haces te dara error
-//         <Fragment>
-//         <h1>Barbara!</h1>
-//         <p>La mejor programadora</p>
-//         </Fragment>
+//         <>
+//         <h1>{newName}</h1>
+//         <p>Soy la mejor programadora</p>
+//         </>
 //     );
-//     }
+// }
 
-//manera de no poner el Fragment <> </>
+const newName = {
+    name: "Barbara",
+    lastName: "Hermosa"
+}
 export const FirstApp = () => {
-        return (
-            <>
-            <h1>Barbara!</h1>
-            <p>La mejor programadora</p>
-            </>
-        );
-        }
+    return (
+        <>
+        <h1>{newName.name}{ newName.lastName}</h1>
+        <p>Soy la mejor programadora</p>
+        </>
+    );
+}
