59d2052f85c658e1db09365450190710846aecce
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-05-01 23:49:35 -0600

    add style titulo y subtitulo como prop numeor

--- 3.Counter-app-vite/src/main.jsx
+++ 3.Counter-app-vite/src/main.jsx
@@ -8,6 +8,6 @@ import './style.css'; //importamos el archivo de estilos css
     ReactDOM.createRoot(document.getElementById("root")).render(
         <React.StrictMode>
             {/* Este es el padre */}
-            <FirstApp title="hola, soy veggeta" subTitle="123"/>
+            <FirstApp title="hola, soy veggeta" subTitle={123}/>
         </React.StrictMode>
     );
