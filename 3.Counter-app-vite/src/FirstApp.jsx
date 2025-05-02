57f3f592c73c2bbacd473c876debab5fd175affa
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-05-01 22:55:44 -0600

    Fragment

--- 3.Counter-app-vite/src/FirstApp.jsx
+++ 3.Counter-app-vite/src/FirstApp.jsx
@@ -1,16 +1,14 @@
+//Fragment Se utiliza para agrupar varios elementos sin envolverlos en un div u otro elemento HTML.
+import { Fragment } from "react";
+
 export const FirstApp = () => {
     return (
-        <h1>My First App!</h1>
+        //si quieres meter varios elementos (parrafos) debes poner div o Fragment si no lo haces te dara error
+        <Fragment>
+        <h1>Barbara!</h1>
+        <p>La mejor programadora</p>
+        </Fragment>
     );
     }
 
-   //export const es con flecha y export default es con funcion normal, ademas el export default solo puede haber uno por archivo y solo se puede a una funcion o clase, mientras que el export se puede usar en varias funciones o clases y no tiene restricciones de nombre.
-
-    // function MiComponente() {
-    //     return <div>Contenido</div>;
-    //   }
-    //   export default MiComponente;
-
-    //   export const MiComponente = () => {
-    //     return <div>Contenido</div>;
-    //   }
+ 
