1edc40f086a4791a11208768d1c0a54f51774600
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-05-01 22:57:47 -0600

    Fragment sin el tetxo

--- 3.Counter-app-vite/src/FirstApp.jsx
+++ 3.Counter-app-vite/src/FirstApp.jsx
@@ -1,14 +1,22 @@
-//Fragment Se utiliza para agrupar varios elementos sin envolverlos en un div u otro elemento HTML.
-import { Fragment } from "react";
+// //Fragment Se utiliza para agrupar varios elementos sin envolverlos en un div u otro elemento HTML.
+// import { Fragment } from "react";
 
+// export const FirstApp = () => {
+//     return (
+//         //si quieres meter varios elementos (parrafos) debes poner div o Fragment si no lo haces te dara error
+//         <Fragment>
+//         <h1>Barbara!</h1>
+//         <p>La mejor programadora</p>
+//         </Fragment>
+//     );
+//     }
+
+//manera de no poner el Fragment <> </>
 export const FirstApp = () => {
-    return (
-        //si quieres meter varios elementos (parrafos) debes poner div o Fragment si no lo haces te dara error
-        <Fragment>
-        <h1>Barbara!</h1>
-        <p>La mejor programadora</p>
-        </Fragment>
-    );
-    }
-
- 
+        return (
+            <>
+            <h1>Barbara!</h1>
+            <p>La mejor programadora</p>
+            </>
+        );
+        }
