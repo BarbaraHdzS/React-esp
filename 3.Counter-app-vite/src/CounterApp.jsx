8a99a5e9777e673065b211fa1b772efebc7fd90e
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-05-02 08:24:23 -0600

    add button[

--- 3.Counter-app-vite/src/CounterApp.jsx
+++ 3.Counter-app-vite/src/CounterApp.jsx
@@ -4,6 +4,10 @@ export const CounterApp = ({value}) => {
         <>
             <h1>CounterApp</h1>
             <h2>{value}</h2>
+{/* //lo que esta a lado del onClikc es una expeersion de javascript, y lo que esta entre llaves es un objeto, por eso se le pone el onClick={} */}
+             <button onClick={() => console.log('+1')}>
+                +1
+             </button>
         </>
     )
 }
