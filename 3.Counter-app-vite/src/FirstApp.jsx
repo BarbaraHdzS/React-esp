4b8346fe26d388a8f16465425f888bd05e6f9a13
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-05-01 23:18:43 -0600

    add style, archivo css

--- 3.Counter-app-vite/src/FirstApp.jsx
+++ 3.Counter-app-vite/src/FirstApp.jsx
@@ -1,26 +1,9 @@
-// //siempre y cuando haya un objeto va a funcionar
-// export const FirstApp = () => { 
-//     return (
-//         <h1>{1+1}</h1>
-//     );
-// }
-
-//se puede todo menos booleanos y null
-// const newName = "Barbara";
-// const newNumber = [1,2,3,4,5,6,7,8,9,10];
-// export const FirstApp = () => {
-//     return (
-//         <>
-//         <h1>{newName}</h1>
-//         <p>Soy la mejor programadora</p>
-//         </>
-//     );
-// }
 
 const newName = {
     name: "Barbara",
     lastName: "Hermosa"
 }
+
 export const FirstApp = () => {
     return (
         <>
@@ -29,3 +12,4 @@ export const FirstApp = () => {
         </>
     );
 }
+
--- 3.Counter-app-vite/src/main.jsx
+++ 3.Counter-app-vite/src/main.jsx
@@ -1,16 +1,13 @@
-//importaciones: react u reactDOM (librerias para crear componentes y renderizarlos en el DOM)
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import { HelloWorldApp } from "./HelloWorldApp";
 import {FirstApp} from "./FirstApp";
+import './style.css'; //importamos el archivo de estilos css
 
 
-    //renderizamos la funcion App en el id root
     ReactDOM.createRoot(document.getElementById("root")).render(
         <React.StrictMode>
             {/* renderizas la funcion de arriba  */}
-            <HelloWorldApp />
             <FirstApp />
         </React.StrictMode>
     );
