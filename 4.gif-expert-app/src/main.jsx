5251ddbf46f727b2481107719becf0e38002ff7d
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-05-05 12:28:48 -0600

    Add styles

--- 4.gif-expert-app/src/main.jsx
+++ 4.gif-expert-app/src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { GifExpertApp } from './GifExpertApp'
 
-
+//para que los estilos se apliquen a toda la aplicacion
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //punto de entrada, solo este 
