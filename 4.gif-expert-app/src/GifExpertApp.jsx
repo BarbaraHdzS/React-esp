b1f4a7260924e59462bb6662b3de9f2844f3e846
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-05-05 13:04:10 -0600

    Add estado de categoria con hook

--- 4.gif-expert-app/src/GifExpertApp.jsx
+++ 4.gif-expert-app/src/GifExpertApp.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react"
+
 export const GifExpertApp = () => {
+//cuando quiero almacenar info y esa info debe cambiar el html, necesito un hook para cambiar el estado (useState)
+//es necesairo poner un valor inicial porque el hook no sabe que valor va a tener
+const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+console.log(categories);
+
     return (
         <>
         {/* titulo */}
@@ -7,6 +15,14 @@ export const GifExpertApp = () => {
          {/* Input */}
 
           {/* Listado de Gif */}
+          <ol>
+                {/* aqui va cada categoria */}
+            {categories.map((category) => {
+                return <li key={category}>{category}</li>
+            })}
+            
+        
+          </ol>
                 {/* Gif Item */}
         </>
     )
