30792cdf33e715188d54cb890e48ec370f1dcedc
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-05-05 14:15:49 -0600

    Add a new category

--- 4.gif-expert-app/src/GifExpertApp.jsx
+++ 4.gif-expert-app/src/GifExpertApp.jsx
@@ -3,7 +3,13 @@ import { useState } from "react"
 export const GifExpertApp = () => {
 //cuando quiero almacenar info y esa info debe cambiar el html, necesito un hook para cambiar el estado (useState)
 //es necesairo poner un valor inicial porque el hook no sabe que valor va a tener
-const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+const [categories, setCategories] = useState(['One Punch',  'Dragon Ball']);
+
+const onAddCategory = ()=> {
+    //categories.push('Barbie') //no se puede hacer esto porque no se actualiza el estado, no se renderiza el html
+    setCategories([...categories,'Barbie']) //esto si se puede hacer porque setCategories es una funcion que actualiza el estado y renderiza el html
+
+}
 
 console.log(categories);
 
@@ -13,6 +19,7 @@ console.log(categories);
         <h1>GifExpertApp</h1>
 
          {/* Input */}
+         <button onClick={onAddCategory}>Agregar</button>
 
           {/* Listado de Gif */}
           <ol>
