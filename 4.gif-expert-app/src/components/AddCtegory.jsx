a58959cd76658e7488470530a8983844756ef5ed
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-05-06 11:32:04 -0600

    Add vento que recarga de pagina en el componente

--- 4.gif-expert-app/src/components/AddCtegory.jsx
+++ 4.gif-expert-app/src/components/AddCtegory.jsx
@@ -4,7 +4,7 @@ export const AddCategory = ()=> {
     //cada componente tiene su propio estado, no se puede compartir el estado entre componentes
 
     const [inputValue, setInputValue] = useState('Barbie');
-
+//el target se refiere al elemento que disparo el evento, y se deconstruye para obtener el value
         const onInputChange = ({target}) => {
        console.log(target.value); //esto es lo que se va a mostrar en el input
         
@@ -12,9 +12,16 @@ export const AddCategory = ()=> {
 
     }
 
+    const onSubmit = (event) => {
+        event.preventDefault(); //esto es para que no se recargue la pagina al enviar el formulario
+        console.log(inputValue); //muetsra en la consola el valor que el usuario escribio 
+    
+    }
 
 
     return (
+        //el form reenvia el evento y recarga la pagina
+        <form onSubmit={(event => onSubmit(event))}>
         <input
             type="text"
             placeholder="Buscar Gif"
@@ -23,7 +30,7 @@ export const AddCategory = ()=> {
             //onChange={(event) => onInputChange(event)} //cuando el input cambie, se va a ejecutar la funcion onInputChange
             onChange={onInputChange}
              />
-            
+            </form>
     )
 }
 // export default AddCategory
