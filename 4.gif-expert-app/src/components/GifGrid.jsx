42ac63cbaa2c8a72d63be6defdf2cf3512debab7
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-05-07 10:12:12 -0600

    Add useEffect para que se renderice una vez por categoria

--- 4.gif-expert-app/src/components/GifGrid.jsx
+++ 4.gif-expert-app/src/components/GifGrid.jsx
@@ -1,7 +1,16 @@
-import {getGifs} from '../helpers/getGifs';
-
+import { useState, useEffect } from 'react';
+import {getGifs} from '../helper/getGifs';
 //en este caso se repetirá el mismo proceso para cada categoría que se pase al componente GifGrid
 export const GifGrid = ({category}) => {
+
+    //estado para almacenar los gifs
+    //Crea una variable de estado llamada counter, que empieza con el valor 10, y una función setCounter que se usa para cambiar ese valor.
+    const [counter, setCounter] = useState(10);
+
+    //Cuando el componente se monte por primera vez, ejecuta la función getGifs(category) una sola vez.
+    useEffect(() => {
+        getGifs(category);
+    }, []);
 
 getGifs(category);
 
