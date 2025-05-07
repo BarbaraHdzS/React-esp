b5704807c19992aa3535b7f1a7fcb7c41b4d2764
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-05-07 14:20:49 -0600

    Add mapeo de imagenes'

--- 4.gif-expert-app/src/components/GifGrid.jsx
+++ 4.gif-expert-app/src/components/GifGrid.jsx
@@ -1,15 +1,19 @@
-import { useState, useEffect } from 'react';
+import {  useState,useEffect } from 'react';
 import {getGifs} from '../helper/getGifs';
 //en este caso se repetirá el mismo proceso para cada categoría que se pase al componente GifGrid
 export const GifGrid = ({category}) => {
 
-    //estado para almacenar los gifs
-    //Crea una variable de estado llamada counter, que empieza con el valor 10, y una función setCounter que se usa para cambiar ese valor.
-    const [counter, setCounter] = useState(10);
+    const [images, setImages] = useState([]);
+    //useState es un hook que permite agregar estado a un componente funcional. En este caso, se está creando una variable de estado llamada images y una función para actualizarla llamada setImages.
 
+    //funcion asincrona que recibe una categoria y devuelve un array de objetos con la información de los gifs
+    const getImages = async () => { 
+        const newImages = await getGifs(category); //llama a la función getGifs y espera a que se resuelva la promesa
+        setImages(newImages); //actualiza el estado con las nuevas imágenes obtenidas
+    }
     //Cuando el componente se monte por primera vez, ejecuta la función getGifs(category) una sola vez.
     useEffect(() => {
-        getGifs(category);
+        getImages(); //llama a la función getImages para obtener las imágenes
     }, []);
 
 getGifs(category);
@@ -17,6 +21,14 @@ getGifs(category);
   return (
     <>
     <h3>{category}</h3>
+
+    <ol>
+    {/* Mapea el array de imágenes y devuelve un elemento <li> para cada imagen. La clave única para cada elemento es image.id, lo que ayuda a React a identificar qué elementos han cambiado, agregado o eliminado. */}
+    {images.map(({id, title}) => (
+        <li key={id}> {title}</li>
+        // <GifItem key={id} {...img} /> //destructuro el objeto img y le paso las props al componente GifItem
+    ))}
+    </ol>
   </>
   )
 }
