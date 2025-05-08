fd9861c4897e8f0d90a30c0e576feae30a1ed417
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-05-08 13:15:05 -0600

    117. cambiar el numero del counter de las 3 propiedades

--- 5.hook-app/src/01-useState/CounterApp.jsx
+++ 5.hook-app/src/01-useState/CounterApp.jsx
@@ -3,19 +3,31 @@ import React, { useState } from 'react';
 export const CounterApp = () => {
 
     //puedes en lugar de inicializar el estado con un valor numerico, inicializarlo con un objeto o un arreglo
-    const [counter, setCounter] = useState({
-        counter: 10,
+    //el counter ahora es un objeto que tiene 3 counters
+    //destructuramos el objeto para obtener los valores de cada uno de los counters
+    const [{counter1, counter2, counter3}, setCounter] = useState({
+        counter1: 10,
         counter2: 20,
         counter3: 30    
-        }    )
+        })
 
     return (
         <>
-            <h1>Counter:{counter}</h1>
+            <h1>counter 1:  {counter1}</h1>
+            <h1>counter 2:  {counter2}</h1>
+            <h1>counter 3:  {counter3}</h1>
 
             <hr />
 
-            <button className="btn" onClick={() => setCounter(counter+1)}>+1</button>
+{/* cuado llamamos el setCounter estamos diciendo que el nuevo valor del counter es el objeto que le pasamos como argumento, y no el valor anterior */}
+            <button 
+            className="btn" 
+            onClick={() => setCounter({
+                counter1: counter1 + 1,
+                counter2: counter2 + 1,
+                counter3: counter3 + 1,
+             } )
+                }>+1</button>
 
        </>
     )
