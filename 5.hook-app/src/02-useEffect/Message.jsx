e44fcc89eec61560adaeb41827a9d99f7eaf9de0
BarbaraHdzS <barbara.hdz.sanch@gmail.com>
2025-05-08 16:04:04 -0600

    123. importancia de error

--- 5.hook-app/src/02-useEffect/Message.jsx
+++ 5.hook-app/src/02-useEffect/Message.jsx
@@ -4,11 +4,13 @@ import { useEffect, useState } from "react"
 export const Message = () => {
     //podemos mostrar codigo cuando el componente se monta, se actualiza o se desmonta
     //se desmonta para limpiar el efecto secundario
-    const [coords, setCoords] = useState({ x: 0, y: 0});
 
     
+    const [coords, setCoords] = useState({ x: 0, y: 0});
+
+ //se hace el cleanup de la funcion de efecto secundario, para que no se acumulen los eventos y no se pierda memoria
     useEffect(() => {
-        
+        //cuando el componente se crea por primera vez, se ejecuta el efecto secundario
         const onMouseMove = ({ x, y }) => {
             // const coords = { x, y };
             setCoords({ x, y })
@@ -16,6 +18,8 @@ export const Message = () => {
 
         window.addEventListener( 'mousemove', onMouseMove );
         
+        //para remover el efecto secundario, se debe retornar una funcion que se ejecuta cuando el componente se desmonta
+        //si no se hace eso voy a intentar un cambio de estado en un componente que ya no existe y eso marca error
       return () => {
         window.removeEventListener( 'mousemove', onMouseMove );
       }
